c412cb6ec39bf9849a0aa5980a7c34c3f5a2785c
liangzai-alt <ceasar0881@gmail.com>
2025-02-07 06:20:04 +0800

    Update Plugin.php

--- src/aoaostar_com/image_url/Plugin.php
+++ src/aoaostar_com/image_url/Plugin.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace plugin\aoaostar_com\image_url;
+namespace plugin\aoaostar_com\image_url;https://1cafe9f6-preview.lightly.teamcode.com/HTMLProject/AGIgongju.html?server=preview&dcsId=1cafe9f6&token=02qEVn2URPWnRfLUqdWW-Q
 
 interface Plugin
 {
